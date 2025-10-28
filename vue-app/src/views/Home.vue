fd18b86d3c8a2194bca0678287d62301071f6b6a
ArnoultL <150684124+ArnoultL@users.noreply.github.com>
2025-10-28 11:38:45 -0400

    truc

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
-    <div>
-      <KanbanVue/>
-    </div>
+    <KanbanBoard/>
+    <draggable tag="ul" ghost-class="moving-card" class="w-full max-w-md" :list="tasks" :animation="200">
+        <li v-for="task in tasks"
+            :key="task.id">
+            {{task.title}}
+        </li>
+    </draggable>
   </template>
   
   <script>
-  import KanbanVue from '../components/KanbanBoard.vue'
+  import KanbanBoard from "../components/KanbanBoard.vue";
+  import draggable from 'vuedraggable'
   
   export default {
-    components: {
-      KanbanVue
-    }
-  }
+    components: { KanbanBoard, draggable},
+    data (){
+        return {
+            tasks : [
+                { id: 1, title: "Task 1", status : "To Do"},
+                { id: 2, title: "Task 2", status : "Doing"},
+                { id: 3, title: "Task 3", status : "Done"},
+            ]
+        }
+    },
+  };
+
   </script>
+  <style>
+  .moving-card {
+    opacity: 0.5;
+    background-color: #f3f4f6;
+    border: 1px solid #3b82f6;
+  }
+  </style>
