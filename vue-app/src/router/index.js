f9f9e7861863c29d0877606b7b5a5ac08e849b4a
JulieBr14 <mjtjbj05@gmail.com>
2025-11-25 16:13:25 -0500

    started routing authentication work

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -9,18 +9,29 @@ import Tasks from '../views/Tasks.vue'
 import AddTasks from '../views/AddTasks.vue'
 
 const routes = [
-  { path: '/', name: 'Welcome', component: Welcome },
   { path: '/login', name: 'Login', component: Login },
-  {path : '/register', name:'register', component: Register},
-  {path : '/home', name : 'home', component : Home},
-  {path : '/apropos', name : 'welcome', component : Wel},
-  {path : '/addtasks', name: 'addtasks', component: AddTasks},
-  {path : '/tasks/:id', name : 'tasks', component : Tasks, props : true}
+  { path : '/register', name:'register', component: Register },
+  { path: '/', name: 'Welcome', component: Welcome, meta: { auth: false } },
+  { path : '/home', name : 'home', component : Home, meta: { auth: false } },
+  { path : '/apropos', name : 'apropos', component : Wel, meta: { auth: false } },
+  { path : '/addtasks', name: 'addtasks', component: AddTasks, meta: { auth: false } },
+  { path : '/tasks/:id', name : 'tasks', component : Tasks, props: true, meta: { auth: false } }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((nextRoute, to, next) => {
+  const authNeeded = nextRoute.meta.auth
+
+  if (!authNeeded) {
+    // Si l'authentification pour aller sur la route n'est pas requise, alors on autorise la navigation
+    next()
+  }
+
+  next('/login')
 })
 
 export default router
--- vue-app/src/views/Welcome.vue
+++ vue-app/src/views/Welcome.vue
@@ -11,7 +11,7 @@
     <div>
       <router-link
         to="/register"
-        class="text-white bg-gradient-to-r from-blue-800 to-indigo-900 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        class="!text-white bg-gradient-to-r from-blue-800 to-indigo-900 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
       >
         Join us now !
       </router-link>
